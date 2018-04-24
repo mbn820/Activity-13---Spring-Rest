c6604b47f48a2fb9791a28a7102a9ebb6775ac56
Mikhail Nunez <mikhailbnunez@gmail.com>
2018-04-24 16:44:23 +0800

    Save

--- core/service/src/main/java/com/exist/ecc/core/service/PersonService.java
+++ core/service/src/main/java/com/exist/ecc/core/service/PersonService.java
@@ -5,24 +5,24 @@ import com.exist.ecc.core.dao.PersonDao;
 import com.exist.ecc.core.dao.HibernateDao;
 import java.util.List;
 
-public class PersonDtoService {
+public class PersonService {
 
 	public Integer addPerson(PersonDto personDto) {
 		Person personToBeAdded = new MapperUtil().mapToPerson(personDto);
 		return new PersonDao().addPerson(personToBeAdded);
 	}
 
-	public PersonDto getPersonDto(int id) {
+	public PersonDto getPerson(int id) {
 		Person person = new PersonDao().getPerson(id);
 		return new MapperUtil().mapToPersonDto(person);
 	}
 
-	public List<PersonDto> getAllPersonDto(String orderBy) {
+	public List<PersonDto> getAllPerson(String orderBy) {
 		List<Person> persons = new PersonDao().getAllPerson(orderBy);
 		return new MapperUtil().mapToPersonDtoList(persons);
 	}
 
-	public List<PersonDto> getPersonDtosByLastName(String lastName) {
+	public List<PersonDto> getPersonsByLastName(String lastName) {
 		List<Person> persons = new PersonDao().getPersonsByLastName(lastName);
 		return new MapperUtil().mapToPersonDtoList(persons);
 	}
@@ -37,6 +37,6 @@ public class PersonDtoService {
 	}
 
 	public boolean databaseIsEmpty() {
-		return getAllPersonDto("id").size() == 0;
+		return getAllPerson("id").size() == 0;
 	}
 }
