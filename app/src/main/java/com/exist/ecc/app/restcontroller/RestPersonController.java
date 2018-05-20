f31eed13a8536cbd4ce7dd44e7266c067ffc1054
Mikhail B. Nunez <mikhailbnunez@gmail.com>
2018-05-20 20:10:16 +0800

    Todo: fix post requests

--- app/src/main/java/com/exist/ecc/app/restcontroller/RestPersonController.java
+++ app/src/main/java/com/exist/ecc/app/restcontroller/RestPersonController.java
@@ -15,6 +15,7 @@ import com.exist.ecc.core.model.dto.ContactDto;
 import com.exist.ecc.core.model.dto.UsersDto;
 import com.exist.ecc.core.service.PersonService;
 import com.exist.ecc.core.service.RoleService;
+import com.exist.ecc.core.service.UserService;
 import org.springframework.ui.ModelMap;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
@@ -28,7 +29,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.validation.BindingResult;
 import org.springframework.http.HttpStatus;
@@ -39,28 +44,45 @@ public class RestPersonController {
     private PersonService personService;
 
     @Autowired
+    private UserService userService;
+
+    @Autowired
     public void setPersonService(PersonService personService) {
         this.personService = personService;
     }
 
-    @GetMapping("/restHello")
-    public String restHello() {
-        return "Hello!@";
-    }
-
-    @GetMapping("/restAllPersons")
+    @GetMapping("/rest/persons")
     public List listOfAllPersons() {
         return personService.getAllPerson("id");
     }
 
     @GetMapping("/rest/person/{id}")
-    public ResponseEntity person(@PathVariable("id") int id) {
+    public ResponseEntity getPerson(@PathVariable("id") int id) {
         return new ResponseEntity(personService.getPerson(id), HttpStatus.OK);
     }
 
-    @GetMapping("/restUser")
-    public UsersDto getUsers() {
-        return new UsersDto("mbn", "exist", "ROLE_ADMIN");
+    @PostMapping("/rest/person")
+    public ResponseEntity addPerson(@RequestBody PersonDto person) {
+        personService.addPerson(person);
+        return new ResponseEntity(person, HttpStatus.OK);
     }
+
+    @DeleteMapping("rest/person/{id}")
+    public ResponseEntity deletePerson(@PathVariable("id") int id) {
+        personService.deletePerson(id);
+        return new ResponseEntity(id, HttpStatus.OK);
+    }
+
+    @GetMapping("rest/user/{id}")
+    public UsersDto getUser(@PathVariable("id") int id) {
+        return userService.getUser(id);
+    }
+
+    @PostMapping("rest/user")
+    public void addUser(@RequestBody UsersDto user) {
+        userService.addUser(user);
+    }
+
+
 
 }
