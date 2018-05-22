38c4c18b54d30ea8ecd48b1f85ba364b74266722
Mikhail Nunez <mikhailbnunez@gmail.com>
2018-05-22 11:09:25 +0800

    Finished SprngMvcConfig class

--- app/src/main/java/com/exist/ecc/app/config/SecurityWebApplicationInitializer.java
+++ app/src/main/java/com/exist/ecc/app/config/SecurityWebApplicationInitializer.java
@@ -1,3 +1,5 @@
+package com.exist.ecc.app.config;
+
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
--- app/src/main/java/com/exist/ecc/app/config/SpringMvcConfig.java
+++ app/src/main/java/com/exist/ecc/app/config/SpringMvcConfig.java
@@ -1,41 +1,66 @@
-// package com.exist.ecc.app.config;
-//
-// import org.springframework.context.MessageSource;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.ComponentScan;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-// import org.springframework.web.servlet.ViewResolver;
-// import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-// import org.springframework.web.servlet.view.InternalResourceViewResolver;
-// import org.springframework.web.servlet.view.JstlView;
-//
-// @EnableWebMvc
+package com.exist.ecc.app.config;
+
+import java.util.Locale;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.handler.MappedInterceptor;
+
+@Configuration
+@EnableWebMvc
 // @ComponentScan(basePackages = "com.exist.ecc")
-// public class SpringMvcConfig extends WebMvcConfigurerAdapter{
-//
-//     @Bean
-//     public ViewResolver viewResolver() {
-//         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-//         viewResolver.setViewClass(JstlView.class);
-//         viewResolver.setPrefix("/WEB-INF/pages/");
-//         viewResolver.setSuffix(".jsp");
-//         return viewResolver;
-//     }
-//
-// 	@Bean
-//     public MessageSource messageSource() {
-//         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-//         messageSource.setBasename("messages");
-//         messageSource.setDefaultEncoding("UTF-8");
-//         return messageSource;
-//     }
-//
-//     @Override
-//     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
-//         configurer.enable();
-//     }
-//
-// }
+public class SpringMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+	@Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale( new Locale("en") );
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Bean
+    public MappedInterceptor mappedInterceptor() {
+        return new MappedInterceptor( null, localeChangeInterceptor() );
+    }
+
+    // @Override
+    // public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+    //     configurer.enable();
+    // }
+
+}
